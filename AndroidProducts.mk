41c87ca36b01200a853d343c49748fe72ed45efa
aseelps <aseelpsathar@gmail.com>
2021-01-11 02:16:56 +0530

    vince: Bringup DotOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,10 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_vince.mk
+    $(LOCAL_DIR)/dot_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_vince-user \
-    derp_vince-userdebug \
-    derp_vince-eng
+    dot_vince-user \
+    dot_vince-userdebug \
+    dot_vince-eng
+
