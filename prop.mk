c90a9382e8eeb7acfc044d6bf3c0503450277d3c
SunnyRaj84348 <sunnyraj7461@gmail.com>
2021-02-27 12:55:04 +0530

    vince:Add some tweaks for improved ram management
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- prop.mk
+++ prop.mk
@@ -297,3 +297,15 @@ pm.sleep_mode=1
 ro.ril.power_collapse=1
 ro.ril.fast.dormancy.rule=0
 persist.sys.shutdown.mode=hibernate
+
+#Better Ram
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
